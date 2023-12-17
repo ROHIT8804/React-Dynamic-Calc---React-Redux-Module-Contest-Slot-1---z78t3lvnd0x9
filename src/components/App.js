e4f6650c012a840498109198988e7ee0a9e94935
Rohit Kumar <37514410+ROHIT8804@users.noreply.github.com>
2023-12-17 15:33:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,28 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
-const [number1,setNum1] = useState('');
-const [number2,setNum2] = useState('');
-  const [res,setRes] = useState('');
+  const [number1, setNum1] = useState('');
+  const [number2, setNum2] = useState('');
+  const [res, setRes] = useState(0); // Changed to a number instead of string
 
-  const handleAddition = ()=>{
-    const sum = parseInt(number1) + parseInt(number2);
+  const handleAddition = () => {
+    const sum = parseInt(number1 || 0, 10) + parseInt(number2 || 0, 10); // Parsing numbers, handling empty strings
     setRes(sum);
   };
-  
+
   return (
     <div id="main">
-      <input id='input1' value={number1} onChange={(e)=>setNum1(e.target.value)}/>
-       +
-      <input id='input2' value={number2} onChange={(e)=>setNum2(e.target.value)}/> 
+      <input id='input1' value={number1} onChange={(e) => setNum1(e.target.value)} />
+      +
+      <input id='input2' value={number2} onChange={(e) => setNum2(e.target.value)} />
+
+      {/* Displaying the result */}
+      <p id='result'>{res}</p>
       
-      <p id='result'>{handleAddition()}</p>
+      {/* Triggering addition */}
+      <button onClick={handleAddition}>Add</button>
     </div>
-  )
+  );
 }
 
 
