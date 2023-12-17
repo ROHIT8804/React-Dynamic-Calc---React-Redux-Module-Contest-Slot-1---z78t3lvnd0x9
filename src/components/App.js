01a93aeadf659f59f33443b06a45d19ce6c7a24d
Rohit Kumar <37514410+ROHIT8804@users.noreply.github.com>
2023-12-17 10:11:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+const [number1,setNum1] = useState('');
+const [number2,setNum2] = useState('');
+  const [res,setRes] = useState('');
 
+  const handleAddition = ()=>{
+    const sum = parseInt(number1) + parseInt(number2);
+    setRes(sum);
+  };
+  
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' onChange={(e)=>setNum1(e.target.value)}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' onChange={(e)=>setNum2(e.target.value)}/> 
       
-      <p id='result'></p>
+      <p id='result'>{handleAddition()}</p>
     </div>
   )
 }
